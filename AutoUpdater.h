7344b7f7d95ae96b19104036579e50d0bfcb1a28
Vadim Peretokin <vperetokin@gmail.com>
2017-11-01 08:16:42 +0100

    Revert autoupdater.h

--- AutoUpdater.h
+++ AutoUpdater.h
@@ -1,31 +1,13 @@
 /*
- * autoupdater.h
- * Copyright 2016, Thorbjørn Lindeijer <bjorn@lindeijer.nl>
- *
- * This file is part of Tiled.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (C) 2008 Remko Troncon
  */
 
-#pragma once
-
-#include <QDateTime>
+#ifndef AUTOUPDATER_H
+#define AUTOUPDATER_H
 
 class AutoUpdater
 {
 public:
-    AutoUpdater();
     virtual ~AutoUpdater();
 
     virtual void checkForUpdates() = 0;
@@ -35,11 +17,6 @@ public:
 
     virtual void setAutomaticallyDownloadsUpdates(bool on) = 0;
     virtual bool automaticallyDownloadsUpdates() = 0;
-
-    virtual QDateTime lastUpdateCheckDate() = 0;
-
-    static AutoUpdater *instance() { return sInstance; }
-
-private:
-    static AutoUpdater *sInstance;
 };
+
+#endif
